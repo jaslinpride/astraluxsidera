f32e3749ab1de1eaf17aebb8ea9c9dd556d09a30
Teriana Jaslin Pride <48602876+jaslinpride@users.noreply.github.com>
2025-04-20 05:07:53 -0400

    Add files via upload

--- home.js
+++ home.js
@@ -1,6 +1,6 @@
 const mainMenu = document.querySelector('.mainMenu');
-const openMenu = document.querySelector('openMenu');
-const closeMenu = document.querySelector('closeMenu');
+const openMenu = document.querySelector('.openMenu');
+const closeMenu = document.querySelector('.closeMenu');
 
 
 
