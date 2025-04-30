97d900f04b3a089f1ad9ae3eadb6eb58e7bada51
Teriana Jaslin Pride <48602876+jaslinpride@users.noreply.github.com>
2025-04-30 18:21:33 -0400

    Add files via upload

--- administrativeconcerns.js
+++ administrativeconcerns.js
@@ -32,3 +32,20 @@ function slideRight() {
         behavior: "smooth"
     });
 }
+
+const sections = document.querySelectorAll('.hidden');
+window.addEventListener('scroll', checkSections);
+
+function checkSections() {
+    const triggerBottom = window.innerHeight * 0.8;
+
+    sections.forEach(section => {
+        const sectionTop = section.getBoundingClientRect().top;
+
+        if (sectionTop < triggerBottom) {
+            section.classList.add('show');
+        } else {
+            section.classList.remove('show');
+        }
+    });
+}
